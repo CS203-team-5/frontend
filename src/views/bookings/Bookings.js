365aa8bc1fe6c9f31cb7350fb77794a4e7c920ea
Nicole Lim <nicole.lim.2020@scis.smu.edu.sg>
2021-09-22 18:40:24 +0800

    create calendar in bookings

--- src/views/bookings/Bookings.js
+++ src/views/bookings/Bookings.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Calendar from '../components/calendar/Calendar'
+import 'react-calendar/dist/Calendar.css'
+
 const Bookings = () => {
+    
     return (
-        <div></div>
+        
+        <div style={{backgroundColor:"red"}}>
+            <Calendar />
+        </div>
     )
 }
 
