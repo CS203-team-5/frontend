c0d67089e5221fb7dbed92579e593b14954ed25c
Nicole Lim <nicole.lim.2020@scis.smu.edu.sg>
2021-09-22 19:35:31 +0800

    create calendar in bookings tab

--- src/views/bookings/Bookings.js
+++ src/views/bookings/Bookings.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Calendar from '../components/calendar/Calendar'
+import pict from './../../assets/images/calender/calendar_icon.png'
 import 'react-calendar/dist/Calendar.css'
-
 const Bookings = () => {
     
     return (
         
         <div style={{backgroundColor:"red"}}>
+            <h1 className='text-left'>
+            <img className='text-centre' src={pict} size="md" /> Calendar
+            <h1 className='text-right' style={{float: "right", border: '2px solid black'}}> 3/10 days remaining </h1>
+            </h1>
             <Calendar />
         </div>
     )
--- src/views/components/calendar/Calendar.js
+++ src/views/components/calendar/Calendar.js
@@ -1,13 +1,11 @@
 import { React, useState } from 'react';
 import Calendar1 from 'react-calendar';
-import pict from '../../../assets/images/calender/calendar_icon.png'
 
 const Calendar = () => {
     const [date, setDate] = useState(new Date());
 
     return (
         <div className='app'>
-            <h1 className='text-left'> <img src={pict} size="md" /> Calendar</h1>
             <div className='calendar-container'>
                 <Calendar1 onChange={setDate} value={date} />
             </div>
