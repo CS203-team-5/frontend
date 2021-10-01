fb81fa38b6de2d574230bf8facbc688f9c88fb0d
Jing Yuan <jyphee.2020@scis.smu.edu.sg>
2021-10-01 17:09:32 +0800

    JY2

--- src/views/bookings/Bookings.js
+++ src/views/bookings/Bookings.js
@@ -2,16 +2,64 @@ import React from 'react'
 import Calendar from '../components/calendar/Calendar'
 import pict from './../../assets/images/calender/calendar_icon.png'
 import 'react-calendar/dist/Calendar.css'
-const Bookings = () => {
-    
+import { NavDropdown } from 'react-bootstrap'
+import {
+    CContainer,
+    CHeader,
+    CHeaderBrand,
+    CHeaderDivider,
+    CHeaderNav,
+    CHeaderToggler,
+    CNavLink,
+    CNavItem,
+} from '@coreui/react'
+
+// import Calendar from 'react-calendar-pane';
+// import moment, { calendarFormat } from 'moment';
+// import date from 'react-calendar-pane';
+
+const Bookings = (props) => {
+
+    console.log("Message is ", props);
     return (
-        
-        <div style={{backgroundColor:"red"}}>
-            <h1 className='text-left'>
-            <img className='text-centre' src={pict} size="md" /> Calendar
-            <h1 className='text-right' style={{float: "right", border: '2px solid black'}}> 3/10 days remaining </h1>
-            </h1>
-            <Calendar />
+        <div>
+            {/* left div */}
+            <div style={{ width: "751px", border: "1px solid red", float: "left" }}>
+                <div >
+                    <h1 className='text-left'>
+                        <img className='text-centre' src={pict} size="md" /> Calendar
+                        <div style={{ float: "right" }}>
+                            <p className='text-right' style={{ fontSize: '18px' }}> Days remaining </p>
+                            <p className='text-right' style={{ textAlign: "center", fontSize: '20px' }}> 3/10 </p>
+                        </div>
+                    </h1>
+                </div>
+                <Calendar />
+            </div>
+            {/* right div */}
+            <div style={{ border: "1px solid blue", textAlign: "center" }}>
+                <h1 >Date </h1>
+                {/* <Calendar date={moment("23/10/2015", "DD/MM/YYYY")} onSelect={this.onSelect} />  */}
+                <h4 style={{
+                    height: "50px", width: "1", border: "1px solid black",
+                }}>
+                    3rd September 2021</h4>
+
+                <h1 >Location</h1>
+                <div style={{ border: "1px solid purple", float: "center" }}>
+                    <CNavItem style={{float:"right"}}>
+                        <NavDropdown title="Location" id="nav-dropdown">
+                            <NavDropdown.Item href="/records">Level 1</NavDropdown.Item>
+                            <NavDropdown.Item href="/admin/news">Level 2</NavDropdown.Item>
+                        </NavDropdown>
+                    </CNavItem>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/views/components/calendar/Calendar.js
+++ src/views/components/calendar/Calendar.js
@@ -1,20 +1,48 @@
-import { React, useState } from 'react';
-import Calendar1 from 'react-calendar';
+// import { React, useState } from 'react';
+// import Calendar1 from 'react-calendar';
 
-const Calendar = () => {
-    const [date, setDate] = useState(new Date());
+// const Calendar = () => {
+//     const [date, setDate] = useState(new Date());
+
+//     return (
+//         <div className='app' >
+//             <div className='calendar-container'>
+//                 <Calendar1 onChange={setDate} value={date} />
+//             </div>
+//             <p className='text-left'>
+//                 <span className='bold'>Selected Date:</span>{' '}
+//                 {date.toDateString()}
+//             </p>
+//         </div>
+//     )
+// }
+
+// export default Calendar
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import Bookings from 'src/views/bookings/Bookings';
+// import './Sample.less';
+
+export default function Calendar1() {
+    const [value, onChange] = useState(new Date());
+    console.log("Calender value is ", value);
+    <Bookings name={value} />
 
     return (
-        <div className='app'>
-            <div className='calendar-container'>
-                <Calendar1 onChange={setDate} value={date} />
+        <div className="Sample">
+            <div className="Sample__container">
+                <main className="Sample__container__content">
+                    <Calendar
+                        onChange={onChange}
+                        showNeighboringMonth={false}
+                        minDate={new Date()}
+                        value={value}
+                    />
+                    <p className='text-left'>
+                        <span className='bold'>Selected Date:</span>{' '}
+                    </p>
+                </main>
             </div>
-            <p className='text-left'>
-                <span className='bold'>Selected Date:</span>{' '}
-                {date.toDateString()}
-            </p>
         </div>
-    )
+    );
 }
-
-export default Calendar
