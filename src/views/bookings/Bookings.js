e11658e438759c10259257bccff041b37ec80672
Nicole Lim <nicole.lim.2020@scis.smu.edu.sg>
2021-10-17 19:04:29 +0800

    changed all Cross Origin to a proxy to map backend to frontend

--- src/views/bookings/Bookings.js
+++ src/views/bookings/Bookings.js
@@ -1,40 +1,39 @@
-import React, { Component } from 'react'
-import Calendar from '../components/calendar/Calendar'
+import React, {Component} from 'react'
+import Calendar1 from '../components/calendar/Calendar'
 import pict from './../../assets/images/calender/calendar_icon.png'
 import 'react-calendar/dist/Calendar.css'
-import { NavDropdown } from 'react-bootstrap'
-import { Calendar1 } from '../components/calendar/Calendar'
+import {NavDropdown} from 'react-bootstrap'
 
 class Bookings extends React.Component {
+
   render() {
     console.log('Message is ', this.props)
     return (
-      <div style={{ display: 'table', margin: 'auto' }}>
+      <div style={{display: 'table', margin: 'auto'}}>
         {/* left div */}
         <div
-          style={{ width: '751px', border: '1px solid red', float: 'left', marginRight: '64px' }}
+          style={{width: '751px', border: '1px solid red', float: 'left', marginRight: '64px'}}
         >
           <div>
             <h1 className="text-left">
-              <img className="text-centre" src={pict} size="md" /> Calendar
-              <div style={{ float: 'right' }}>
-                <p className="text-right" style={{ fontSize: '18px' }}>
+              <img className="text-centre" src={pict} size="md"/> Calendar
+              <div style={{float: 'right'}}>
+                <p className="text-right" style={{fontSize: '18px'}}>
                   {' '}
                   Days remaining{' '}
                 </p>
-                <p className="text-right" style={{ textAlign: 'center', fontSize: '20px' }}>
+                <p className="text-right" style={{textAlign: 'center', fontSize: '20px'}}>
                   {' '}
                   3/10{' '}
                 </p>
               </div>
             </h1>
           </div>
-          <Calendar />
+          <Calendar1 style={{width: "750px"}}/>
         </div>
         {/* right div */}
         <div
-          style={{ border: '1px solid blue', textAlign: 'center', float: 'right', padding: '12px' }}
-        >
+          style={{border: '1px solid blue', textAlign: 'center', float: 'right', padding: '12px'}} className = 'card'>
           <h1>Date </h1>
           {/* <Calendar date={moment("23/10/2015", "DD/MM/YYYY")} onSelect={this.onSelect} />  */}
           <h4
@@ -49,7 +48,7 @@ class Bookings extends React.Component {
 
           <h1>Location</h1>
           <div>
-            <NavDropdown title="Level 1" id="nav-dropdown" style={{ float: 'center' }}>
+            <NavDropdown title="Level 1" id="nav-dropdown" style={{float: 'center'}}>
               <NavDropdown.Item href="">Level 2</NavDropdown.Item>
               <NavDropdown.Item href="">Level 3</NavDropdown.Item>
             </NavDropdown>
--- src/views/components/calendar/Calendar.js
+++ src/views/components/calendar/Calendar.js
@@ -1,30 +1,8 @@
-// import { React, useState } from 'react';
-// import Calendar1 from 'react-calendar';
-
-// const Calendar = () => {
-//     const [date, setDate] = useState(new Date());
-
-//     return (
-//         <div className='app' >
-//             <div className='calendar-container'>
-//                 <Calendar1 onChange={setDate} value={date} />
-//             </div>
-//             <p className='text-left'>
-//                 <span className='bold'>Selected Date:</span>{' '}
-//                 {date.toDateString()}
-//             </p>
-//         </div>
-//     )
-// }
-
-// export default Calendar
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import Bookings from 'src/views/bookings/Bookings';
-// import './Sample.less';
 
 export default function Calendar1() {
-    
     const [value, onChange] = useState(new Date());
     console.log("Calender value is ", value);
     <Bookings name={value} />
@@ -39,9 +17,6 @@ export default function Calendar1() {
                         minDate={new Date()}
                         value={value}
                     />
-                    {/* <p className='text-left'>
-                        <span className='bold'>Selected Date:</span>{' '}
-                    </p> */}
                 </main>
             </div>
         </div>
