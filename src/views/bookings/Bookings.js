9ca27e98efee31ade7fd344c2125a6e1a807d912
juzli_ <lixiang.lxw@gmail.com>
2021-10-17 23:13:48 +0800

    booking looks

--- src/views/bookings/Bookings.js
+++ src/views/bookings/Bookings.js
@@ -3,60 +3,76 @@ import Calendar1 from '../components/calendar/Calendar'
 import pict from './../../assets/images/calender/calendar_icon.png'
 import '../components/calendar/Calendar.css'
 import {NavDropdown} from 'react-bootstrap'
+import {
+  CAvatar,
+  CButton,
+  CButtonGroup,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CCol,
+  CForm,
+  CFormCheck,
+  CFormInput,
+  CFormLabel,
+  CFormSelect,
+  CInputGroup,
+  CInputGroupText,
+  CProgress,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+import { CChartLine } from '@coreui/react-chartjs'
+import { getStyle, hexToRgba } from '@coreui/utils'
+import CIcon from '@coreui/icons-react'
 
 class Bookings extends React.Component {
 
   render() {
     console.log('Message is ', this.props)
     return (
-      <div style={{display: 'table', margin: 'auto'}}>
-        {/* left div */}
-        <div
-          style={{width: '751px', border: '1px solid red', float: 'left', marginRight: '64px'}}
-        >
-          <div>
-            <h1 className="text-left">
-              <img className="text-centre" src={pict} size="md"/> Calendar
-              <div style={{float: 'right'}}>
-                <p className="text-right" style={{fontSize: '18px'}}>
-                  {' '}
-                  Days remaining{' '}
-                </p>
-                <p className="text-right" style={{textAlign: 'center', fontSize: '20px'}}>
-                  {' '}
-                  3/10{' '}
-                </p>
-              </div>
-            </h1>
-          </div>
-          <Calendar1 style={{width: "750px"}}/>
-        </div
-          style={{width: '751px', border: '1px solid red', float: 'left', marginRight: '64px'}}>
-        {/* right div */}
-        <div
-          style={{border: '1px solid blue', textAlign: 'center', float: 'right', padding: '12px'}} className = 'card'>
-          <h1>Date </h1>
-          {/* <Calendar date={moment("23/10/2015", "DD/MM/YYYY")} onSelect={this.onSelect} />  */}
-          <h4
-            style={{
-              height: '50px',
-              width: '1',
-              border: '',
-            }}
-          >
-            3rd September 2021
-          </h4>
+      <>
+        <CRow>
+          <CCol xs>
+            <CCard className="mb-4">
+              <CCardHeader>Submit New Booking</CCardHeader>
+              <CCardBody>
+                <CRow>
+                  <CCol xs={12} md={6} xl={6}>
+                    <Calendar1/>
+                  </CCol>
 
-          <h1>Location</h1>
-          <div>
-            <NavDropdown title="Level 1" id="nav-dropdown" style={{float: 'center'}}>
-              <NavDropdown.Item href="">Level 2</NavDropdown.Item>
-              <NavDropdown.Item href="">Level 3</NavDropdown.Item>
-            </NavDropdown>
-          </div>
-          <div></div>
-        </div>
-      </div>
+                  <CCol xs={12} md={6} xl={6}>
+                    <CForm>
+                      <CRow className="mb-3">
+                        <CFormLabel htmlFor="inputEmail3" className="col-sm-4 col-form-label">Date Left</CFormLabel>
+                        <CFormLabel htmlFor="inputEmail3" className="col-sm-4 col-form-label">3/10</CFormLabel>
+                      </CRow>
+                      <CRow className="mb-3">
+                        <CFormLabel htmlFor="inputEmail3" className="col-sm-4 col-form-label">Date Selected</CFormLabel>
+                        <CCol sm={8} >
+                          <CFormInput type="text" id="dateSelected" value="17/12/2021"/>
+                        </CCol>
+                      </CRow>
+                      <CButton type="submit">Submit</CButton>
+                    </CForm>
+                  </CCol>
+                </CRow>
+
+                <br />
+
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      </>
+
     )
   }
 }
