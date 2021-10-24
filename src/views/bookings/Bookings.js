e7e5ee69b7e99f7a73806269807ecb6c37b0a250
Jing Yuan <jyphee.2020@scis.smu.edu.sg>
2021-10-24 19:28:15 +0800

    Made changes to the date dates

--- src/views/bookings/Bookings.js
+++ src/views/bookings/Bookings.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import pict from './../../assets/images/calender/calendar_icon.png'
 import '../components/calendar/Calendar.css';
 import Axios from 'axios';
+import dayjs from 'dayjs';
+
 import {
   CButton,
   CCard,
@@ -20,16 +22,19 @@ function Bookings(props) {
   const [date, setDate] = useState(new Date());
   const locale = 'en-SG';
   const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric"
+    // weekday: "long",
+    // year: "numeric",
+    // month: "long",
+    // day: "numeric"
+    timeZone: "Japan"
   };
 
   const url = "http://localhost:8080/api/bookings/emp/"
 
   function submit(e) {
     e.preventDefault();
+    console.log("Before ", date);
+    date.setHours(8);
     Axios.post(url, {
       bdate: date,
       status: "Completed",
@@ -38,12 +43,11 @@ function Bookings(props) {
       }
     })
       .then(res => {
-        console.log(res.date)
       })
   }
 
   function handle(e) {
-    const newdata = {...date}
+    const newdata = { ...date }
     newdata[e.target.id] = e.target.value
     setDate(newdata)
     console.log(newdata)
@@ -53,7 +57,7 @@ function Bookings(props) {
     <CRow>
       <CCol xs>
         <CCard className="mb-4">
-          <CCardHeader component="h5"><img src={pict}/> Booking Form </CCardHeader>
+          <CCardHeader component="h5"><img src={pict} /> Booking Form </CCardHeader>
           <CCardBody>
             <CRow>
               <CCol xs={12} md={6} xl={6}>
@@ -77,15 +81,15 @@ function Bookings(props) {
                     <CFormLabel htmlFor="inputEmail3" className="col-sm-4 col-form-label"> Email </CFormLabel>
                     <CCol sm={8}>
                       <CFormInput type="email" id="b_email" onChange={event => setEmail(event.target.value)}
-                                  placeholder="Enter your email"/>
+                        placeholder="Enter your email" />
 
                     </CCol>
                   </CRow>
                   <CRow className="mb-3">
                     <CFormLabel htmlFor="inputEmail3" className="col-sm-4 col-form-label"> Date Selected </CFormLabel>
                     <CCol sm={8}>
-                      <CFormInput type="string" id="b_date" value={date.toLocaleDateString(locale, options)}
-                                  onChange={setDate} placeholder="b_date" disabled="disabled"/>
+                      <CFormInput type="string" id="b_date" value={date.getDate() +"/"+ date.getMonth()+"/"+date.getFullYear()}
+                        onChange={setDate} placeholder="b_date" disabled="disabled" />
                     </CCol>
                   </CRow>
                   <CButton type="submit"> Submit </CButton>
@@ -93,7 +97,7 @@ function Bookings(props) {
               </CCol>
             </CRow>
 
-            <br/>
+            <br />
 
           </CCardBody>
         </CCard>
