9bcbba57576ffb87ad55d3de0d75cfe36e6a11ce
Jing Yuan <jyphee.2020@scis.smu.edu.sg>
2021-11-12 21:52:26 +0800

    Fixed default page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const loading = (
   </div>
 )
 // Containers
-const DefaultLayout = React.lazy(() => import('./views/pages/login/Login'))
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 
 // Pages
 const Login = React.lazy(() => import('./views/pages/login/Login'))
@@ -29,20 +29,16 @@ class App extends Component {
 
         <React.Suspense fallback={loading}>
 
-          <switch>
+          <Route exact path="/Login" name="Login Page" render={(props) => <Login {...props} />} />
 
+          <Route exact path="/404" name="Page 404" render={(props) => <Page404 {...props} />} />
+          <Route exact path="/500" name="Page 500" render={(props) => <Page500 {...props} />} />
+          <Route exact path="/CheckIn" name="CheckIn" render={(props) => <CheckIn{...props} />} />
 
-            <Route exact path="/Login" name="Login Page" render={(props) => <Login {...props} />} />
+          <Route exact path="/ForgetPassword" name="ForgetPassword" render={(props) => <ForgetPassword{...props} />} />
+          <Route exact path="/UpdatePassword" name="UpdatePassword" render={(props) => <UpdatePassword{...props} />} />
+          <Route path="/" name="Home" render={(props) => <DefaultLayout {...props} />} />
 
-            <Route exact path="/404" name="Page 404" render={(props) => <Page404 {...props} />} />
-            <Route exact path="/500" name="Page 500" render={(props) => <Page500 {...props} />} />
-            <Route exact path="/CheckIn" name="CheckIn" render={(props) => <CheckIn{...props} />} />
-
-            <Route exact path="/ForgetPassword" name="ForgetPassword" render={(props) => <ForgetPassword{...props} />} />
-            <Route exact path="/UpdatePassword" name="UpdatePassword" render={(props) => <UpdatePassword{...props} />} />
-            <Route path="/" name="Home" render={(props) => <DefaultLayout {...props} />} />
-
-          </switch>
         </React.Suspense>
       </BrowserRouter>
     )
